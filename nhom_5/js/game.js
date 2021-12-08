c73a38d9badc150244a7e85794534233b9b09108
Giang Huynh <trgianghuynhdev1808@gmail.com>
2021-12-08 22:48:37 +0700

    nhom_5 | implement move pawn piece

--- nhom_5/js/game.js
+++ nhom_5/js/game.js
@@ -6,16 +6,21 @@ function Game() {
   };
 
   this.showChessBoard = function () {
+    const self = this;
     const boxes = this.board.boxes;
     const boardElement = document.getElementById("chess-board");
+
+    boardElement.innerHTML = "";
 
     boxes.forEach((row, rowIndex) => {
       const rowElement = document.createElement("div");
       rowElement.className = "row";
-      for (const spot of row) {
+
+      row.forEach((spot, colIndex) => {
         const spotElement = document.createElement("div");
         spotElement.className = "square";
         spotElement.style.backgroundColor = spot.y % 2 === 0 ? "white" : "gray";
+        spotElement.id = `spot-${rowIndex}-${colIndex}`;
 
         if (rowIndex % 2 !== 0) {
           spotElement.style.backgroundColor =
@@ -28,26 +33,77 @@ function Game() {
           const imgElement = document.createElement("img");
           imgElement.setAttribute("src", currentPiece.imgUrl);
           imgElement.className = "piece-image";
+          imgElement.id = `piece-${rowIndex}-${colIndex}`;
 
           // add event listener for "click" piece
-          imgElement.addEventListener("click", () => {
-            onClickPieceElement(spot);
+          imgElement.addEventListener("click", function () {
+            onClickPieceElement(self, spot);
           });
 
           spotElement.appendChild(imgElement);
+        } else {
+          // add event listener for "click" spot
+          spotElement.addEventListener("click", (event) => {
+            onClickSpotElement(event, self, spot);
+          });
         }
 
         rowElement.appendChild(spotElement);
-      }
+      });
+
       boardElement.appendChild(rowElement);
     });
   };
 }
 
-function onClickPieceElement(spot) {
+function onClickPieceElement(self, spot) {
+  resetCanMovesStyle();
   const currentPiece = spot.piece;
 
-  const spotPositions = currentPiece.getSpotsCanMove(spot.x, spot.y);
+  const currentPieceElement = document.getElementById(
+    `piece-${spot.x}-${spot.y}`
+  );
+  currentPieceElement.classList.add("piece--selected");
+  self.board.setCurrentSpotSelected(spot);
 
-  console.log({ spotPositions });
+  const spotPositions = currentPiece.getSpotsCanMove(
+    spot.x,
+    spot.y,
+    self.board.boxes
+  );
+  spotPositions.forEach((position) => {
+    //update Spot style
+    const currentSpotElement = document.getElementById(
+      `spot-${position.x}-${position.y}`
+    );
+    currentSpotElement.classList.add("square--can-move");
+
+    // set can move for Spot
+    const currentSpot = self.board.getSpotFromBoxes(position.x, position.y);
+    currentSpot.setCanMove(true);
+  });
+}
+
+function onClickSpotElement(event, self, spot) {
+  const selectedSpot = self.board.currentSpotSelected;
+  const currentSpotElement = event.target;
+
+  if (
+    selectedSpot &&
+    currentSpotElement.classList.contains("square--can-move")
+  ) {
+    self.board.setPieceForSpot(selectedSpot.piece, spot.x, spot.y);
+    //remove old chess
+    self.board.setPieceForSpot(null, selectedSpot.x, selectedSpot.y);
+
+    handleForPawn(selectedSpot.piece);
+    self.board.setCurrentSpotSelected(null);
+    self.showChessBoard();
+  }
+}
+
+function handleForPawn(piece) {
+  if (piece.name === PieceName.PAWN && piece.isFirstStep) {
+    piece.setIsFirstStep(false);
+  }
 }
--- nhom_5/js/pieces/pawn.js
+++ nhom_5/js/pieces/pawn.js
@@ -4,25 +4,30 @@ function Pawn(isWhite, isKilled = false) {
   Piece.call(this, isWhite, isKilled, PieceName.PAWN, pawnImgUrl);
   this.isFirstStep = true;
 
-  this.getSpotsCanMove = function (startX, startY) {
+  this.getSpotsCanMove = function (startX, startY, boxes) {
     let spots = [];
 
     if (this.isWhite) {
       spots.push({ x: startX - 1, y: startY });
 
-      if (this.isFirstStep) {
+      if (this.isFirstStep && boxes[startX - 1][startY].piece === null) {
         spots.push({ x: startX - 2, y: startY });
       }
+    } else {
+      spots.push({ x: startX + 1, y: startY });
 
-      return spots;
+      if (this.isFirstStep && boxes[startX + 1][startY].piece === null) {
+        spots.push({ x: startX + 2, y: startY });
+      }
     }
 
-    spots.push({ x: startX + 1, y: startY });
+    const filterSpots = spots.filter((spot) => {
+      return boxes[spot.x][spot.y].piece === null;
+    });
 
-    if (this.isFirstStep) {
-      spots.push({ x: startX + 2, y: startY });
-    }
-
-    return spots;
+    return filterSpots;
+  };
+  this.setIsFirstStep = function (newFirstStep) {
+    this.isFirstStep = newFirstStep;
   };
 }

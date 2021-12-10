964e94bde04d598d9c66adc8c7cfccf8663e4ae7
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2021-12-10 13:37:24 +0700

    nhom 1 | select and move pieces

--- nhom_1/src/rook.js
+++
@@ -1,7 +0,0 @@
-import { COLUMNS, ROWS, Side, PieceName } from './variables.js';
-import {Piece} from './piece.js';
-
-export const Rook = function (color, isKilled = false) {
-  const backgroundUrl = color === Side.BLACK ? '/nhom_1/assets/img/rook_black.png' : '/nhom_1/assets/img/rook_white.png'
-  Piece.call(this, PieceName.ROOK, color, isKilled, backgroundUrl);
-}

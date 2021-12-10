fda29d5646abf4fe2efbeaf4761eb5966f3e3fb4
NAM HOÀNG QUỐC <20127566@student.hcmus.edu.vn>
2021-12-10 14:45:19 +0700

    nhom_2 | move chess

--- nhom_2/chessboard.js
+++ nhom_2/chessboard.js
@@ -1,70 +1,137 @@
-const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
-letters.reverse();
-
 function ChessBoard() {
-  //property
-  this.table = document.createElement("table");
-  this.table.id = "chessboard";
+  let state = GAME_STATE.NEWGAME;
+  this.selectedSquare = null;
+  let secondClick = false;
+  let turn;
+
+  // board - chessboard in HTML
+  this.boardHTML = document.createElement("div");
+  Object.assign(this.boardHTML, {
+      id: 'chessboard__board',
+      style: `width : ${SQUARE_SIZE * NUMBER_SQUARE + "px"};
+              height : ${SQUARE_SIZE * NUMBER_SQUARE + "px"};`
+  });
+
+  // map - chessboard in JS
+  this.chessMap;
 
   // set
-  this.setNewChessBoard = () => {
-    for (let x = 8; x >= 1; x--) {
-      let row = document.createElement("tr");
-      for (let y = 8; y >= 1; y--) {
-        let color = ((y + x) % 2 != 0) ? "rgb(163, 119, 84)" : "rgb(243, 235, 215)";
-        let cell = new Cell(letters[y - 1] , x, color);
-        let chesspiece = null;
-        switch (x) {
-          case 1:
-          case 8:
-            let color = x == 1 ? "white" : "black";
-            switch (letters[y - 1]) {
-              case "a":
-              case "h":
-                chesspiece = new Rook(color);
-                break;
-              case "b":
-              case "g":
-                chesspiece = new Knight(color);
-                break;
-              case "c":
-              case "f":
-                chesspiece = new Bishop(color);
-                break;
-              case "d":
-                chesspiece = new Queen(color);
-                break;
-              case "e":
-                chesspiece = new King(color);
-                break;
-            }
-            break;
-          case 2:
-            chesspiece = new Pawn("white");
-            break;
-          case 7:
-            chesspiece = new Pawn("black");
-            break;
-        }
-        if (chesspiece != null) {
-          cell.setChesspiece(chesspiece);
-        }
-        row.appendChild(cell.getCell());
+  this.setNewChessMap = () => {
+      // tao chess map thanh mot array chứa các object là các cell
+      this.chessMap = [];
+      turn = ColorType.TEAM.WHITE;
+      for (let i = 0; i < NUMBER_SQUARE; i++) {
+          let row = [];
+          for (let j = 0; j < NUMBER_SQUARE; j++) {
+              let square = new Square(j, i, SQUARE_SIZE);
+              let tile = square.getTile();
+              // select chessman
+              tile.addEventListener('click', () => {
+                  if (!secondClick) {
+                      // select the chess
+                      if (square.havingChessMan() && square.getChessman().getColor() === turn) {
+                          secondClick = true;
+                          square.select(true);
+                          this.selectedSquare = square;
+                      }
+                  }
+                  else {
+                      // xong first click
+                      // move the chess
+                      if (!square.havingChessMan()) {
+                          // to: empty square
+                          secondClick = false;
+                          moveChess(this.selectedSquare, square);
+                          turn = (turn === ColorType.TEAM.WHITE) ? ColorType.TEAM.BLACK : ColorType.TEAM.WHITE;
+
+                      }
+                      else if (this.selectedSquare.getChessman().getColor() !== square.getChessman().getColor()) {
+                          // to: enemy
+                          secondClick = false;
+                          moveChess(this.selectedSquare, square);
+                          turn = (turn === ColorType.TEAM.WHITE) ? ColorType.TEAM.BLACK : ColorType.TEAM.WHITE;
+
+                      }
+                      else {
+                          // to: ally -> select again
+                          square.select(true);
+                          this.selectedSquare.select(false);
+                          this.selectedSquare = square;
+                          secondClick = true;
+                      }
+
+                  }
+              })
+              row.push(square);
+          }
+          this.chessMap.push(row);
       }
-      this.table.appendChild(row);
-    }
   }
 
+  this.setNewChessBoard = () => {
+      for (let i = 0; i < NUMBER_SQUARE; i++) {
+          for (let j = 0; j < NUMBER_SQUARE; j++) {
+              let chessman = null;
+              if (i == 6) {
+                  chessman = new Pawn(ColorType.TEAM.WHITE);
+              }
+              if (i == 1) {
+                  chessman = new Pawn(ColorType.TEAM.BLACK);
+              }
+              if (i == 0 || i == 7) {
+                  let color = (i == 7) ? ColorType.TEAM.WHITE : ColorType.TEAM.BLACK;
+                  switch (j) {
+                      case 0:
+                      case 7:
+                          chessman = new Rook(color)
+                          break;
+                      case 1:
+                      case 6:
+                          chessman = new Bishop(color);
+                          break;
+                      case 2:
+                      case 5:
+                          chessman = new Knight(color);
+                          break;
+                      case 3:
+                          chessman = new Queen(color);
+                          break;
+                      case 4:
+                          chessman = new King(color);
+                          break;
+                  }
+              }
+              if (chessman != null) {
+                  this.chessMap[i][j].setChessman(chessman);
+              }
+              this.boardHTML.appendChild(this.chessMap[i][j].getTile());
+          }
+      }
+  }
+
+  // event handler
+
+  this.handleSelect = () => {
+
+  }
+
+  //method
+  function moveChess(source, destination) {
+      let chessman = source.getChessman();
+      source.select(false);
+      source.removeChessman();
+      destination.setChessman(chessman);
+  }
 
   // render
-  this.render = function () {
-    let x = document.getElementById('main');
-    x.appendChild(this.table);
-    this.setNewChessBoard();
+  this.render = () => {
+      if (state == GAME_STATE.NEWGAME) {
+          this.setNewChessMap();
+          this.setNewChessBoard();
+          state = GAME_STATE.PLAYING;
+      }
+      document.body.appendChild(this.boardHTML);
   }
+
 }
-
-
-const chessBoard = new ChessBoard();
-chessBoard.render(); // create board
 
---
+++ nhom_2/chessman.js
@@ -0,0 +1,63 @@
+function ChessMan() {
+  const CHESSMAN_SIZE = "72px";
+  this.image = document.createElement("img");
+  this.image.style.width = CHESSMAN_SIZE;
+  this.image.style.height = CHESSMAN_SIZE;
+  this.position_X;
+  this.position_Y;
+  this.color;
+  // set
+  this.setImage = (type, color) => {
+      this.image.src = `/assets/chess-${type}-${color}.png`;
+      this.color = color;
+  }
+
+  this.setPosition = (posX, posY) => {
+      this.position_X = posX;
+      this.position_Y = posY;
+  }
+
+  // get
+  this.getImage = () => {
+      return this.image;
+  }
+
+  this.getColor = () => {
+      return this.color;
+  }
+
+}
+
+function Pawn(color) {
+  ChessMan.call(this);
+  this.setImage('pawn', color);
+}
+
+function Bishop(color) {
+  ChessMan.call(this);
+  this.setImage('bishop', color);
+}
+
+function King(color) {
+  ChessMan.call(this);
+  this.setImage('king', color);
+}
+
+function Queen(color) {
+  ChessMan.call(this);
+  this.setImage('queen', color);
+}
+
+function Rook(color) {
+  ChessMan.call(this);
+  this.setImage('rook', color);
+}
+
+function Knight(color) {
+  ChessMan.call(this);
+  this.setImage('knight', color);
+}
+
+
+
+

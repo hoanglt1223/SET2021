18c94542575a69c7f5db95033d854c33acd9295d
Hoang Phan Le <phanle11vn@gmail.com>
2021-12-12 17:21:35 +0700

    nhom_5 | implement knight moves

--- nhom_5/js/pieces/knight.js
+++ nhom_5/js/pieces/knight.js
@@ -2,4 +2,28 @@ function Knight(isWhite, isKilled = false) {
   const knightImgUrl = isWhite ? './img/knight_white.png' : './img/knight_black.png'
 
   Piece.call(this, isWhite, isKilled, PieceName.KNIGHT, knightImgUrl)
+
+  this.getSpotsCanMove = function (startX, startY, board) {
+    let spots = [];
+  
+      
+      spots.push({ x: startX - 2, y: startY - 1});
+      spots.push({ x: startX + 2, y: startY - 1});
+      spots.push({ x: startX - 2, y: startY + 1});
+      spots.push({ x: startX + 2, y: startY + 1});
+      spots.push({ x: startX - 1, y: startY - 2});
+      spots.push({ x: startX + 1, y: startY - 2});
+      spots.push({ x: startX - 1, y: startY + 2});
+      spots.push({ x: startX + 1, y: startY + 2});
+      
+      const filterInValidSpots = spots.filter((spot) => {
+        return (spot.x >= 0 && spot.x < ROWS)
+          && (spot.y >= 0 && spot.y < COLS)
+      })
+  
+      const filterSpots = filterInValidSpots.filter((spot)=>{
+        return !board.checkHasPieceOnSpot(spot.x, spot.y)
+      })
+      return filterSpots
+    }
 }

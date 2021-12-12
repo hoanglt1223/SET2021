922ef5d2a14f8f3e18370bcc63796e11fe6483a1
NAM HOÀNG QUỐC <20127566@student.hcmus.edu.vn>
2021-12-12 14:59:06 +0700

    nhom_2 | reset new game HTML

--- nhom_2/gamePlay.js
+++ nhom_2/gamePlay.js
@@ -2,18 +2,14 @@
 let chessboard = new ChessBoard();
 chessboard.render();
 let resultBoard = document.getElementById("resultBoard");
-resultBoard.style.display = "none";
 
 
 function gameOver(colorWin){
-    resultBoard.style.display ="inline";
+    resultBoard.style.visibility = "visible";
     let newgameButton = document.getElementById("newgame_button");
     let congratulation = document.getElementById("congratulation");
 
     congratulation.innerHTML = "Congratulation !";
-    debugger;
-    let teamWin = congratulation.lastChild
-    teamWin.innerHTML = colorWin;
     newgameButton.addEventListener("click", () => {
         chessboard.render();
         resultBoard.style.display = "none";

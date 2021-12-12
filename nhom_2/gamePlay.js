dbef764de583653aa59168746b93e9085101e28c
NAM HOÀNG QUỐC <20127566@student.hcmus.edu.vn>
2021-12-12 17:29:44 +0700

    nhom_2 | show pawm when wi HTML

--- nhom_2/gamePlay.js
+++ nhom_2/gamePlay.js
@@ -11,7 +11,15 @@ function gameOver(colorWin){
 
     congratulation.innerHTML = "Congratulation !";
     let teamwin = document.getElementById("teamwin");
-    teamwin.innerHTML = colorWin.toUpperCase();
+    let colorTeam = teamwin.getElementsByClassName("colorTeam")[0];
+    let imageTeam = teamwin.getElementsByClassName("imageTeam")[0];
+
+    colorTeam.innerHTML = colorWin.toUpperCase();
+    Object.assign(imageTeam, {
+        src: `./assets/chess-pawn-${colorWin}.png`,
+        style: `width: 80px;
+                height: 80px;`
+    })
     newgameButton.addEventListener("click", () => {
         chessboard.render();
         resultBoard.style.display = "none";

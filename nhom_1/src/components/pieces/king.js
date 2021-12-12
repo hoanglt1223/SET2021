6534bd5ee311a675927a2664e71c5ea208b9bb5b
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2021-12-12 23:52:56 +0700

    nhom_1 | king moves

--- nhom_1/src/components/pieces/king.js
+++ nhom_1/src/components/pieces/king.js
@@ -1,7 +1,25 @@
-import {Side, PieceName } from '../../constants.js';
+import {Side, PieceName, COLUMNS, ROWS} from '../../constants.js';
 import {Piece} from './piece.js';
 
 export const King = function (color, column, row, isSelected = false, isKilled = false) {
   const backgroundUrl = color === Side.BLACK ? '/nhom_1/assets/img/king_black.png' : '/nhom_1/assets/img/king_white.png'
   Piece.call(this, PieceName.KING,color, column, row, isSelected, isKilled, backgroundUrl);
+
+  this.possibleMoves = () => {
+    const indexOfCol = COLUMNS.indexOf(this.column);
+    const indexOfRow = ROWS.indexOf(this.row);
+    let res = [];
+    let queenDirectionsMatrix = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, -1], [-1, 1]];
+    queenDirectionsMatrix.forEach(direction => {
+      const squareId = COLUMNS[indexOfCol + direction[0]] + ROWS[indexOfRow + direction[1]];
+      if (document.getElementById(squareId)?.getElementsByClassName(this.color).length > 0) {
+        return;
+      } else if(document.getElementById(squareId)?.getElementsByClassName(this.color === Side.WHITE ? Side.BLACK : Side.WHITE).length > 0){
+        res.push(squareId);
+        return;
+      }
+      res.push(squareId);
+    })
+    return res;
+  };
 }
--- nhom_1/src/components/pieces/queen.js
+++ nhom_1/src/components/pieces/queen.js
@@ -1,8 +1,28 @@
-import {Side, PieceName } from '../../constants.js';
+import {Side, PieceName, COLUMNS, ROWS } from '../../constants.js';
 import {Piece} from './piece.js';
 
 export const Queen = function (color, column, row, isSelected = false, isKilled = false) {
   const backgroundUrl = color === Side.BLACK ? '/nhom_1/assets/img/queen_black.png' : '/nhom_1/assets/img/queen_white.png'
   Piece.call(this, PieceName.QUEEN,color, column, row, isSelected, isKilled, backgroundUrl);
+
+  this.possibleMoves = () => {
+    const indexOfCol = COLUMNS.indexOf(this.column);
+    const indexOfRow = ROWS.indexOf(this.row);
+    let res = [];
+    let queenDirectionsMatrix = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, -1], [-1, 1]];
+    queenDirectionsMatrix.forEach(direction => {
+      for (let i = 1; i < 8; i++) {
+        const squareId = COLUMNS[indexOfCol + direction[0] * i] + ROWS[indexOfRow + direction[1] * i];
+        if (document.getElementById(squareId)?.getElementsByClassName(this.color).length > 0) {
+          break;
+        } else if(document.getElementById(squareId)?.getElementsByClassName(this.color === Side.WHITE ? Side.BLACK : Side.WHITE).length > 0){
+          res.push(squareId);
+          break;
+        }
+        res.push(squareId);
+      }
+    })
+    return res;
+  };
 }
 

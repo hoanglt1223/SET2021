1fc48a3685736ad56a7268bb9b74bfb413a0496e
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2021-12-18 23:06:49 +0700

    quangphuc | path

--- quangphuc/index.js
+++ quangphuc/index.js
@@ -4,10 +4,15 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    const path = req.url.split('?')[0];
+    if(/\/tasks/g.test(path)) {
+        console.log('task page');
+    } else if(/\/image/g.test(path)){
+        console.log('image page');
+    }
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-    console.log(req);
 });
 
 server.listen(port, hostname, () => {

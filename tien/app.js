8be193736067bd5deb895478e64bd6f2ba49e43a
Tiensp19 <tiendr06@gmail.com>
2021-12-29 17:30:16 +0700

    tien | update router and add controller layer

--- tien/app.js
+++ tien/app.js
@@ -1,148 +1,31 @@
 const http = require('http');
-require('dotenv').config();
-const axios = require('axios');
+const routers = require('./routers/routers');
 
-
-
-let hostname = process.env.HOST;
-let port = process.env.PORT;
-
-
-
-let tasks = [
-  {
-    id: process.env.ID1,
-    name: process.env.NAME1,
-    isDeleted: false
-  },
-  {
-    id: process.env.ID2,
-    name: process.env.NAME2,
-    isDeleted: false
-  },
-  {
-    id: process.env.ID3,
-    name: process.env.NAME3,
-    isDeleted: false
-  },
-  {
-    id: process.env.ID4,
-    name: process.env.NAME4,
-    isDeleted: false
-  }
-]
-function findIds() {
-  let ids = [];
-  tasks.forEach((task) => {
-    if (!task.isDeleted) {
-      ids.push(task.id);
-    }
-  })
-  return ids;
-}
-
-function handleGet(url, res, req) {
-  let ids = findIds();
-  let getStarted = false;
-  if (url === "/people") {
-    return res.end(JSON.stringify(tasks));
-  } else {
-    ids.forEach((id) => {
-      if (url == "/people/:" + id) {
-        let taskUpload = tasks.filter((task) => {
-          return task.id === id;
-        })
-        res.end(JSON.stringify(taskUpload));
-      } else if (url !== "/people" && url !== "/people/:" + id) {
-        getStarted = true;
-      }
-    })
-  }
-}
-
-function handleDelete(url, res, req) {
-  let ids = findIds();
-  let isDeleted = false;
-  ids.forEach((id) => {
-    if (url == "/tasks/:" + id) {
-      tasks.forEach((task) => {
-        if (task.id === id) {
-          task.isDeleted = true;
-        }
-      })
-      isDeleted = true;
-    }
-  })
-  if (isDeleted) {
-    res.end("Delete task successfully");
-  } else {
-    res.end("Run http://127.0.0.1:3001/tasks/: + id of task to delete task ");
-  }
-}
-
-function handlePost(url, res, req) {
-  if (url == "/tasks") {
-    axios
-      .post('http://127.0.0.1:3001/tasks', {
-        todo: 'Buy the milk'
-      })
-      .then(res => {
-        console.log(`statusCode: ${res.status}`)
-        console.log(res)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-
-  } else {
-    res.end("Run http://127.0.0.1:3001/tasks with post to save task data on server");
-  }
-}
-
-function handlePut(url, res, req) {
-  let ids = findIds();
-  let success = false;
-  ids.forEach((id) => {
-    if (url == "/tasks/:" + id) {
-
-      success = true;
-    }
-  })
-  if (success) {
-    res.end("Put task successfully");
-  } else {
-    res.end("Run http://127.0.0.1:3001/tasks/: + id of task to put task ");
-  }
-}
-
-function routing(res, req) {
-    const routeUrl = req.url;
-    const routers = {
-        GET: {
-            '/task': getTask,
-            '/image': getImage
-        },
-        POST: {
-            '/task': postTask,
-        },
-        PUT: {
-            '/task': putTask,
-        },
-        DELETE: {
-            'task': deleteTask,
-        }
-    };
-
-    routers[req.method](res, req);
-}
+const hostname = '127.0.0.1';
+const port = 3000;
 
 // define server  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
-  return routing(res, req);
+
+  const requestUrl = req.url.split("?")[0]; //get url
+  const router = getRouter(requestUrl);     //get router
+
+  return router[req.method][requestUrl](req, res);  //execute handle method
 });
+
+// get router method
+function getRouter(requestUrl) {
+  switch(requestUrl) {
+    case "/tasks":
+      return routers.tasks;
+    case "/image":
+      return routers.image;
+    default:
+      return {};
+  }
+}
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ tien/controller/tasksController.js
@@ -0,0 +1,30 @@
+/* ======== TASKS CONTROLLER ======== */
+const taskController = {
+    getTasks,
+    postTasks,
+    putTasks,
+    deleteTasks,
+    patchTasks
+};
+
+function getTasks(req, res) {
+    
+}
+
+function postTasks(req, res) {
+
+}
+
+function putTasks(req, res) {
+
+}
+
+function deleteTasks(req, res) {
+
+}
+
+function patchTasks(req, res) {
+
+}
+
+module.exports = taskController;

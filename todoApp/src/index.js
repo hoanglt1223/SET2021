b65822b21997164f07c1f52057f4e306341dd2ef
NAM HOÀNG QUỐC <20127566@student.hcmus.edu.vn>
2022-01-21 18:47:15 +0700

    create backend

--- todoApp/src/index.js
+++ todoApp/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ProjectManager from "./components/projectManager/index.js";
 import './style.css'
-
+const axios = require('axios')
 
 ReactDOM.render(<ProjectManager/>, document.getElementById('root'));

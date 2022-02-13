ed0de59821195bc0f628cc698c2ed2f51a2e200d
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2022-02-14 00:22:25 +0700

    quangphuc |get data from api

--- quang-phuc/web/src/App.js
+++ quang-phuc/web/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios.post(`http://localhost:8080/get-tasks`)
+      .then(res => {
+        const tasks = res.data;
+        setData({ tasks });
+      })
+  },[])
+  console.log(data);
   return (
     <div className="l-app">
       <header className="l-app__header">
         Todo App
       </header>
+      <p>
+        {JSON.stringify(data)}
+      </p>
     </div>
   );
 }

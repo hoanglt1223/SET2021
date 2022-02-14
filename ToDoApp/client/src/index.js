13830ab18234b3c5c3afdec8ac64b02a3dfabbc1
NAM HOÀNG QUỐC <20127566@student.hcmus.edu.vn>
2022-02-14 14:31:20 +0700

    nhom 2 | bug

--- ToDoApp/client/src/index.js
+++ ToDoApp/client/src/index.js
@@ -1,31 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ProjectManager from "./components/projectManager/index.js";
-import UserManager from "./components/userManager/index.js";
 import './style.css'
-// import axios from "axios"
+import axios from "axios"
+import ProjectManager from './components/projectManager'
 
 
-// const uriServer = "http://localhost:5500/projects";
+const uriServer = "http://localhost:5500/projects";
 
 
-// axios.get(uriServer, JSON.stringify({}))
-//     .then((response) => {
-//         ReactDOM.render(<ProjectManager
-//             projectFetched = {response.data}
-//         />, document.getElementById('root'));
-//     })
+axios.get(uriServer, JSON.stringify({}))
+  .then((response) => {
+    ReactDOM.render(<ProjectManager
+      projectFetched={response.data}
+    />, document.getElementById('root'));
+  })
 
 
-import { LogInContextProvider } from "./components/login/loginContext.js";
-
-import Login from "./components/login"
-
-
-
-ReactDOM.render(
-  // <LogInContextProvider>
-    <Login/>
-  // </LogInContextProvider>,
-  ,document.getElementById("root")
-);

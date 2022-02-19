d8e737c9bd6b83a6aef978d0222b2f51e1b65a24
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2022-02-19 21:50:24 +0700

    quangphuc | (fix): fix users page

--- quang-phuc/web/src/routes/users/[id]/index.jsx
+++ quang-phuc/web/src/routes/users/[id]/index.jsx
@@ -5,48 +5,9 @@ import DataContext from "../../../contexts/data.context";
 import {taskService} from "../../../services";
 
 function User() {
-  const [tasks, setTasks] = useState([]);
-  const [isAllDataLoading, setIsAllDataLoading] = useContext(DataContext.context);
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function getTaskFromDatabase() {
-    setIsLoading(true);
-    const dataFromDatabase = await taskService.getAllTasks();
-    setTasks(dataFromDatabase);
-    setIsLoading(false);
-  }
-
-  useEffect(async () => {
-    await getTaskFromDatabase()
-  },[]);
-
-
   return (
-    <div className="l-app">
-      <header className="l-app__header">
-        Todo App
-      </header>
-      {
-        isLoading && (<div className="spinner-border mt-5" role="status">
-          <span className="visually-hidden"/>
-        </div>)
-      }
-      <div className="container mt-5">
-        <AddTaskForm />
-        <div className="row border-2 border-dark border-bottom p-2">
-          <strong className="col-1">Done</strong>
-          <strong className="col-5 text-start"><div className="ps-4">Task Name</div></strong>
-          <strong className="col-3 text-start"><div className="ps-4">Owner</div></strong>
-          <strong className="col-3">Action</strong>
-        </div>
-        {
-          tasks.map(task => (
-            <TaskRow task={task}/>)
-          )
-        }
-      </div>
-    </div>
-  );
+    <><h1>User page</h1></>
+  )
 }
 
 export default User;
--- quang-phuc/web/src/routes/users/index.jsx
+++ quang-phuc/web/src/routes/users/index.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import DataContext from "../../contexts/data.context";
+import {taskService} from "../../services";
+import AddTaskForm from "../../components/AddTaskForm";
+import TaskRow from "../../components/TaskRow";
 
 function Users(props) {
+  const [tasks, setTasks] = useState([]);
+  const [isAllDataLoading, setIsAllDataLoading] = useContext(DataContext.context);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function getTaskFromDatabase() {
+    setIsLoading(true);
+    const dataFromDatabase = await taskService.getAllTasks();
+    setTasks(dataFromDatabase);
+    setIsLoading(false);
+  }
+
+  useEffect(async () => {
+    await getTaskFromDatabase()
+  },[]);
+
+
   return (
-    <><h1>Users Page</h1></>
-  )
+    <div className="l-app">
+      <header className="l-app__header">
+        Todo App
+      </header>
+      {
+        isLoading && (<div className="spinner-border mt-5" role="status">
+          <span className="visually-hidden"/>
+        </div>)
+      }
+      <div className="container mt-5">
+        <AddTaskForm />
+        <div className="row border-2 border-dark border-bottom p-2">
+          <strong className="col-1">Done</strong>
+          <strong className="col-5 text-start"><div className="ps-4">Task Name</div></strong>
+          <strong className="col-3 text-start"><div className="ps-4">Owner</div></strong>
+          <strong className="col-3">Action</strong>
+        </div>
+        {
+          tasks.map(task => (
+            <TaskRow task={task}/>)
+          )
+        }
+      </div>
+    </div>
+  );
 }
 
 export default Users;
